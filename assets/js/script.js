2920046eb224cd2891940084fa9ea4e7590e6a49
Kaveen2001 <managekaveen2001@gmail.com>
2023-05-10 00:55:33 +0530

    Java Script SPA Assignment Completed

---
+++ assets/js/script.js
@@ -0,0 +1,127 @@
+/*Hide all content*/
+/*$("#customerContent").css("display","none");*/
+
+/*Display all content*/
+/*$("#itemContent").css("display","block");*/
+
+$("#sliderContent").css("display","block");
+$("#foodsectionContent").css("display","block");
+$("#catalouge").css("display","block");
+$("#desertsectionContent").css("display","block");
+$("#carddesertContent").css("display","block");
+
+$("#customerContent").css("display","none");
+
+$("#itemContent").css("display","none");
+
+$("#invoiceContent").css("display","none");
+$("#paymentsContent").css("display","none");
+$("#addtocartContent").css("display","none");
+
+$("#footersectionContent").css("display","block");
+
+
+/*Dashboard Section*/
+$("#linkDashboard").click(function () {
+
+    $("#sliderContent").css("display","block");
+    $("#foodsectionContent").css("display","block");
+    $("#catalouge").css("display","block");
+    $("#desertsectionContent").css("display","block");
+    $("#carddesertContent").css("display","block");
+
+    $("#customerContent").css("display","none");
+
+    $("#itemContent").css("display","none");
+
+    $("#invoiceContent").css("display","none");
+    $("#paymentsContent").css("display","none");
+    $("#addtocartContent").css("display","none");
+
+    $("#footersectionContent").css("display","block");
+
+});
+
+/*Customer Section*/
+$("#linkCustomer").click(function () {
+
+    $("#sliderContent").css("display","none");
+    $("#foodsectionContent").css("display","none");
+    $("#catalouge").css("display","none");
+    $("#desertsectionContent").css("display","none");
+    $("#carddesertContent").css("display","none");
+
+    $("#customerContent").css("display","block");
+
+    $("#itemContent").css("display","none");
+
+    $("#invoiceContent").css("display","none");
+    $("#paymentsContent").css("display","none");
+    $("#addtocartContent").css("display","none");
+
+    $("#footersectionContent").css("display","block");
+
+});
+
+/*Item Section*/
+$("#linkItem").click(function () {
+
+    $("#sliderContent").css("display","none");
+    $("#foodsectionContent").css("display","none");
+    $("#catalouge").css("display","none");
+    $("#desertsectionContent").css("display","none");
+    $("#carddesertContent").css("display","none");
+
+    $("#customerContent").css("display","none");
+
+    $("#itemContent").css("display","block");
+
+    $("#invoiceContent").css("display","none");
+    $("#paymentsContent").css("display","none");
+    $("#addtocartContent").css("display","none");
+
+    $("#footersectionContent").css("display","block");
+
+});
+
+/*Place Order Section*/
+$("#linkOrder").click(function () {
+
+    $("#sliderContent").css("display","none");
+    $("#foodsectionContent").css("display","none");
+    $("#catalouge").css("display","none");
+    $("#desertsectionContent").css("display","none");
+    $("#carddesertContent").css("display","none");
+
+    $("#customerContent").css("display","none");
+
+    $("#itemContent").css("display","none");
+
+    $("#invoiceContent").css("display","block");
+    $("#paymentsContent").css("display","block");
+    $("#addtocartContent").css("display","block");
+
+    $("#footersectionContent").css("display","block");
+
+});
+
+$("#linkPlaceOrder").click(function () {
+
+    $("#sliderContent").css("display","none");
+    $("#foodsectionContent").css("display","none");
+    $("#catalouge").css("display","none");
+    $("#desertsectionContent").css("display","none");
+    $("#carddesertContent").css("display","none");
+
+    $("#customerContent").css("display","none");
+
+    $("#itemContent").css("display","none");
+
+    $("#invoiceContent").css("display","block");
+    $("#paymentsContent").css("display","block");
+    $("#addtocartContent").css("display","block");
+
+    $("#footersectionContent").css("display","block");
+
+});
+
